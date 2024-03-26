74de2d50f5f35cddaf2d1d2369d4eb51fe5310c4
r-agus <r.agustin@alumnos.upm.es>
2024-03-26 12:54:14 +0100

    Added modules to main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # main.py
 import sys
 sys.path.insert(0, './Face_Recognition')
+sys.path.insert(0, './EmotionDetector')
+sys.path.insert(0, './DeepFloyd')
+sys.path.insert(0, './Whisper')
 
 from face_reco_class import FaceRecognizer
 
