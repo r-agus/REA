c2e3ffff5e2bf577ff2ad8538509bd94c4129a4a
Lorena Burgos Delgado <lorenaburgosdelgado53@gmail.com>
2024-03-26 18:23:36 +0100

    Whisper added

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ sys.path.insert(0, './EmotionDetector')
 sys.path.insert(0, './DeepFloyd')
 sys.path.insert(0, './Whisper')
 
-from face_reco_class import FaceRecognizer
-from REA_EmotionDetector import REA_EmotionDetector
+from Face_Recognition.face_reco_class import FaceRecognizer
+from EmotionDetector.REA_EmotionDetector import REA_EmotionDetector
+from Whisper.speech_recognition import SpeechRecognition
 
+
+
+# Se puede lanzar la interfaz
+#sr.crear_interfaz()
+
+sr = SpeechRecognition()
 fr = FaceRecognizer()
 ed = REA_EmotionDetector()
 
@@ -21,3 +28,12 @@ if fr.is_teo(image):
 else:
 
     print(fr.recognize_face(image))
+    
+#TODO EMOTION IMAGE GENERATOR TO ADULT
+
+
+#TRANSCRIPT AUDIO FROM ADULT
+sr.transcribir("audio.mp3", "transcripcion.txt")
+
+    
+#TODO LAST STEP:TEXT TO IMAGE GENERATOR
